06a2edcdc49ae5a30f395f36a97d3111db191815
Anil Bhattarai <anilbhattarai@gmail.com>
2018-09-29 17:27:08 -0400

    Header, navbar added and buttons are styled

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var j = 0;
 const vowels = ['अ', 'आ', 'इ', 'ई', 'उ', 'ऊ', 'ए', 'ऐ', 'ओ', 'औ', 'अं', 'अ:'];
 const romanizedVowels = ['a', 'aa', 'i', 'ee', 'u', 'oo', 'eh', 'eih', 'o', 'au', 'ahm', 'aha'];
 const syllables = ['क', 'ख', 'ग', 'घ', 'ङ', 'च', 'छ', 'ज', 'झ', 'ञ', 'ट', 'ठ', 'ड', 'ढ', 'ण', 'त', 'थ', 'द', 'ध', 'न', 'प', 'फ', 'ब', 'भ', 'म', 'य', 'र', 'ल', 'व', 'स', 'ष', 'श', 'ह', 'क्ष', 'त्र', 'ज्ञ'];
-const romanizedSyllables = ['ka', 'kha', 'ga', 'gha', 'ng', 'cha', 'chha', 'ja', 'jha', 'yan', 'ta', 'tha', 'da', 'dha', 'ana', 'taa', 'thaa', 'daa', 'dhaa', 'na', 'pa', 'pha', 'ba', 'bha', 'ma', 'ya', 'ra', 'la', 'wa', 'sa', 'shaa', 'sha', 'ha', 'chya', 'tra', 'gyan'];
+const romanizedSyllables = ['ka', 'kha', 'ga', 'gha', 'nga', 'cha', 'chha', 'ja', 'jha', 'yan', 'ta', 'tha', 'da', 'dha', 'ana', 'taa', 'thaa', 'daa', 'dhaa', 'na', 'pa', 'pha', 'ba', 'bha', 'ma', 'ya', 'ra', 'la', 'wa', 'sa', 'shaa', 'sha', 'ha', 'chya', 'tra', 'gyan'];
 
 const vowelButton = document.querySelector('#vowel-button');
 const syllableButton = document.querySelector('#syllable-button');
+const learnVowels = document.querySelector('#learn-vowels');
+const learnSyllables = document.querySelector('#learn-syllables');
 
 const vowelSection = document.querySelector('#vowels');
 const syllableSection = document.querySelector('#syllables')
@@ -19,14 +21,21 @@ vowelButton.addEventListener('click', function (event) {
     }
     j = 0;
     vowelSection.style.display = 'grid';
+    learnSyllables.style.display = 'none';
     syllableSection.style.display = 'none';
     const newDiv = document.createElement('div');
     newDiv.classList.add('vowel-list');
     newDiv.textContent = vowels[i] + " " + romanizedVowels[i];
     vowelSection.appendChild(newDiv);
     i++;
-    if (i == vowels.length) {
+    if (i == 13) {
         i = 0;
+        while (vowelList.firstChild) {
+            vowelList.removeChild(vowelList.firstChild)
+        }
+        learnSyllables.style.display = 'block';
+        learnVowels.style.display = 'none';
+
     }
 });
 
@@ -39,11 +48,16 @@ syllableButton.addEventListener('click', function (event) {
     vowelSection.style.display = 'none';
     const newDiv = document.createElement('div');
     newDiv.classList.add('syllable-list')
-    newDiv.textContent = syllables[j];
+    newDiv.textContent = syllables[j] + " " + romanizedSyllables[j];
     syllableSection.appendChild(newDiv);
     j++;
-    if (j === syllables.length) {
+    if (j === 37) {
         j = 0;
+        while (syllableList.firstChild) {
+            syllableList.removeChild(syllableList.firstChild)
+        };
+        learnVowels.style.display = 'block';
+        learnSyllables.style.display = 'none';
     }
 })
 
